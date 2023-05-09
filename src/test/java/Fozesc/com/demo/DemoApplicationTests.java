7480c21df62f3ead42d97b793182772cf352d243
dogogh <quadros1919@gmail.com>
2023-05-09 00:05:07 -0300

    first commit

---
+++ src/test/java/Fozesc/com/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Fozesc.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
